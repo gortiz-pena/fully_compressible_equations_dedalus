fixes a bug in the calculation of dT_ad_TT, cleans up some of the tt_to_ft code

logic/tt_to_ft.py
@@ -10,9 +10,8 @@ import dedalus.public as de
 
 def sort_file_list(f_list):
     """
-    Given a list of .h5 files, this function sorts them and returns a list of sorted files.
+    Sorts a list of Dedalus output .h5 files numerically and returns the sorted list.
     """
-
     files = []
     for f in f_list:
         file_num = int(f.split('.h5')[0].split('_s')[-1])
@@ -21,20 +20,24 @@ def sort_file_list(f_list):
     return sorted_list
 
 
-def load_tt(path, time, Lz):
+def load_tt(path, avg_time, Lz):
     """
-    Loads in data from a TT system
+    Loads profile and scalar data required for TT-to-FT process from a TT system
 
     Inputs:
     -------
-    See tt_to_ft() docstring.
-
-    Lz : float
-         Depth of dedalus domain
+        path : string
+            A string path to the TT simulation to get the state of
+        avg_time : float
+            The amount of simulation avg_time to average profiles and scalar values over from the TT run
+        Lz : float
+             Depth of dedalus domain
     
     """
     tt_dirs = glob.glob('{:s}/*/'.format(path))
     sub_dirs = [d.split('/')[-2] for d in tt_dirs]
+
+    # Get a checkpoint file to load the convective perturbation state from
     if 'final_checkpoint' in sub_dirs:
         final_checks = sort_file_list(glob.glob('{:s}/final_checkpoint/*.h5'.format(path)))
         checkpoint_TT = final_checks[-1]
@@ -42,45 +45,38 @@ def load_tt(path, time, Lz):
         checks = sort_file_list(glob.glob('{:s}/checkpoint/*.h5'.format(path)))
         checkpoint_TT = checks[-1]
 
-    # Figure out how many scalar / profile files are needed
+    # Figure out how many scalar / profile files avg_time spans
     scalar_files  = sort_file_list(glob.glob(  '{:s}/scalar/*.h5'.format(path)))
     profile_files = sort_file_list(glob.glob('{:s}/profiles/*.h5'.format(path)))
-    end_times = [None, None]
 
-    good_scalar_files = []
-    good_profile_files = []
-    n_scalar_writes = 0
-    n_profile_writes = 0
-    for i in range(len(scalar_files)):
-        this_file = scalar_files[-(i+1)]
-        good_scalar_files.append(this_file)
-        with h5py.File(this_file, 'r') as sf:
-            sim_times = sf['scales']['sim_time'][()]
-            n_scalar_writes += len(sim_times)
-            if end_times[0] is None:
-                end_times[0] = sim_times[-1]
-            if end_times[0] - sim_times[0] > time: 
-                break
-    for i in range(len(profile_files)):
-        this_file = profile_files[-(i+1)]
-        good_profile_files.append(this_file)
-        with h5py.File(this_file, 'r') as pf:
-            sim_times = pf['scales']['sim_time'][()]
-            nz = len(pf['scales/z/1.0'][()])
-            n_profile_writes += len(sim_times)
-            if end_times[1] is None:
-                end_times[1] = sim_times[-1]
-            if end_times[1] - sim_times[0] > time: 
-                break
+    # Index 0 is scalar, index 1 is profile
+    end_times  = [None, None]
+    good_files = [[], []]
+    n_writes   = np.zeros(2, dtype=np.int32)
+    for i in range(2):
+        for j in range(len(scalar_files)):
+            if i == 0:
+                this_file = scalar_files[-(j+1)]
+            else:
+                this_file = profile_files[-(j+1)]
+            good_files[i].append(this_file)
+            with h5py.File(this_file, 'r') as f:
+                if i == 1 and j == 0: 
+                    nz = len(f['scales/z/1.0'][()])
+                sim_times = f['scales']['sim_time'][()]
+                n_writes[i] += len(sim_times)
+                if end_times[i] is None:
+                    end_times[i] = sim_times[-1]
+                if end_times[i] - sim_times[0] > avg_time: 
+                    break
+    good_scalar_files  = sort_file_list(good_files[0])
+    good_profile_files = sort_file_list(good_files[1])
 
-    good_scalar_files  = sort_file_list(good_scalar_files)
-    good_profile_files = sort_file_list(good_profile_files)
-
-    #Get average profiles / scalars
+    #Get average profiles / scalars required for TT-to-FT
     profiles = OrderedDict()
-    scalars = OrderedDict()
-    scalar_work_field = np.zeros(n_scalar_writes)
-    profile_work_field = np.zeros((n_scalar_writes, nz))
+    scalars  = OrderedDict()
+    scalar_work_field  = np.zeros(n_writes[0])
+    profile_work_field = np.zeros((n_writes[1], nz))
     for sk in ['Nu', 's_over_cp_z']:
         i = 0
         for this_file in good_scalar_files:
@@ -99,7 +95,7 @@ def load_tt(path, time, Lz):
                 i += N
         profiles[pk] = np.mean(profile_work_field, axis=0)
     
-    scalars['s_over_cp_z'] *= Lz # take it from vol_avg to integral quantity
+    scalars['s_over_cp_z'] *= Lz # volume averaged entropy gradient * Lz = delta S
     return checkpoint_TT, profiles, scalars
 
 def structure_bvp(atmo_class, atmo_args, atmo_kwargs, profiles, scalars):
@@ -119,53 +115,58 @@ def structure_bvp(atmo_class, atmo_args, atmo_kwargs, profiles, scalars):
             A dictionary with scalar values required for the BVP
     """
 
-    dS_TT = scalars['s_over_cp_z']
-    Nu    = scalars['Nu']
-    T1_TT = profiles['T1']
+    # Grab important raw TT values
+    dS_TT        = scalars['s_over_cp_z']
+    Nu           = scalars['Nu']
+    T1_TT        = profiles['T1']
     UdotGradw_TT = profiles['UdotGradw']
 
+    # Construct Dedalus setup
     atmosphere = atmo_class(*atmo_args, **atmo_kwargs)
-    Lz = atmosphere.Lz
-
+    Lz         = atmosphere.Lz
     z_basis = de.Chebyshev('z', len(T1_TT), interval=[0, Lz], dealias=1)
     domain  = de.Domain([z_basis,], grid_dtype=np.float64, comm=MPI.COMM_SELF)
     problem = de.NLBVP(domain, variables = ['dS', 'S', 'ln_rho1', 'M1'])
     atmosphere.build_atmosphere(domain, problem)
-    dS_TT *= atmosphere.Cp #Turn it into dS rather than dS/Cp units.
     z = domain.grid(0)
 
-    #Solve out for Temp profile
-    S0 = domain.new_field()
-    T_TT = domain.new_field()
-    T1_FT = domain.new_field()
-    UdotGradw = domain.new_field()
-    grad_ad = -(atmosphere.g/atmosphere.Cp)
+    # Remove the 1/Cp from dS/Cp
+    dS_TT *= atmosphere.Cp
 
+    # Solve out for Temp profile of the FT case
+    S0        = domain.new_field()
+    T_TT      = domain.new_field()
+    T1_FT     = domain.new_field()
+    UdotGradw = domain.new_field()
+
+    grad_ad    = -(atmosphere.g/atmosphere.Cp)
     T_TT['g']  = atmosphere.T0['g'] + T1_TT
     T_ad       = 1 + grad_ad*(z - Lz)
-    dT_ad_TT   = np.mean(T_TT.interpolate(z=Lz)['g'] - T_TT.interpolate(z=Lz)['g']) - np.abs(grad_ad*Lz)
+    dT_ad_TT   = np.mean(T_TT.interpolate(z=Lz)['g'] - T_TT.interpolate(z=0)['g']) - np.abs(grad_ad*Lz)
     dT_ad_FT   = dT_ad_TT / Nu
     T1_FT['g'] = dT_ad_FT*((T_TT['g'] - T_ad)/dT_ad_TT) + T_ad - atmosphere.T0['g']
 
-    #Get dS0
-    S0['g'] = atmosphere.Cp*((1/atmosphere.ɣ)*np.log(atmosphere.T0['g']) - ((atmosphere.ɣ-1)/atmosphere.ɣ)*np.log(atmosphere.rho0['g']))
-    dS0 = np.mean(S0.interpolate(z=Lz)['g'] - S0.interpolate(z=0)['g'])
+    # Setup S0, other constants
+    S0['g']           = atmosphere.Cp*((1/atmosphere.ɣ)*np.log(atmosphere.T0['g']) - ((atmosphere.ɣ-1)/atmosphere.ɣ)*np.log(atmosphere.rho0['g']))
+    dS0               = np.mean(S0.interpolate(z=Lz)['g'] - S0.interpolate(z=0)['g'])
     evolved_Ra_factor = (dS_TT / dS0)
+    UdotGradw['g']    = UdotGradw_TT
 
-    UdotGradw['g'] = UdotGradw_TT
-
+    # Feed parameters into the problem (many are fed in through atmosphere class)
     problem.parameters['T1_FT'] = T1_FT
     problem.parameters['dS_TT'] = dS_TT
     problem.parameters['UdotGradw_TT'] = UdotGradw
     problem.substitutions['ln_rho0'] = 'log(rho0)'
+
+    # Hydrostatic equilibrium (modified) + mass conservation.
     problem.add_equation("(T0 + T1_FT)*dz(ln_rho1) = -dz(T1_FT) - T1_FT*ln_rho0_z + (dS/dS_TT)*UdotGradw_TT")
     problem.add_equation("dz(M1) = rho0*(exp(ln_rho1) - 1)")
     problem.add_equation("S/Cp = (1/ɣ) * log(T0 + T1_FT) - ((ɣ-1)/ɣ)*(ln_rho0 + ln_rho1)")
     problem.add_equation("dS = right(S) - left(S)")
-
     problem.add_bc(" left(M1) = 0")
     problem.add_bc("right(M1) = 0")
 
+    # Solve NLBVP
     tol = 1e-10
     solver = problem.build_solver()
     dS = solver.state['dS']
@@ -177,12 +178,9 @@ def structure_bvp(atmo_class, atmo_args, atmo_kwargs, profiles, scalars):
         print('pert norm: {} / dS: {:.2e}'.format(np.sum(np.abs(pert)), np.mean(dS['g'])))
 
     dS_FT = np.mean(dS['g'])
-
     FT_Ra_factor = (dS0 / dS_FT)*evolved_Ra_factor
     
     return T1_FT['g'], ln_rho1_FT['g'], dS_FT/dS_TT, dT_ad_FT/dT_ad_TT, FT_Ra_factor
-
-    
 
 
 def tt_to_ft_preliminaries(atmosphere, atmo_args, atmo_kwargs, path, time):